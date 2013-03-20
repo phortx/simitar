ac6a28bd8e8b7ef93d4a6aba80b1ec7a16cd9e4a
phortx <benny@itws.de>
2013-03-20 16:51:38 +0100

    Moar Bugfixes

--- v/0.1/_.js
+++ v/0.1/_.js
@@ -20,14 +20,11 @@ function run() {
 	$head.append('<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.no-icons.min.css" rel="stylesheet" />');
 	$head.append('<link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet" />');
 	$head.append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />');
+	$head.append('<script src="' + basePath + 'marked.js"></script>');
+	$head.append('<script src="' + basePath + 'prettify/prettify.js"></script>');
+	$head.append('<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>');
 
-	$body.append('<script src="' + basePath + 'marked.js"></script>');
-	$body.append('<script src="' + basePath + 'prettify/prettify.js"></script>');
-	$body.append('<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>');
 	$body.prepend('<div class="container"></div>');
-
-	var $container = $('.container');
-
 
 	// Load theme
 	if ($body.attr('class')) {
@@ -43,47 +40,62 @@ function run() {
 	$head.append('<link href="' + basePath + 'prettify/prettify.css" rel="stylesheet" />');
 	$head.append('<link href="' + basePath + 'simitar.css" rel="stylesheet" />');
 
-	window.setTimeout(function() {
-		// Parse markdown
-		marked.setOptions({
-			gfm: true,
-			tables: true,
-			breaks: true,
-			pedantic: false,
-			sanitize: false,
-			smartLists: true,
-			langPrefix: 'language-',
-			highlight: function(code, lang) {
-				if (lang === 'js') {
-					return highlighter.javascript(code);
-				}
-				return code;
+
+	// Workaround for possible loading problems
+	var recurFn = function() {
+		if (marked != undefined && prettyPrint != undefined) {
+			finish();
+		} else {
+			window.setTimeout(recurFn, 100);
+		}
+	}
+
+	recurFn();
+}
+
+function finish() {
+	var $body = $('body'),
+		$container = $('.container');
+	
+	// Parse markdown
+	marked.setOptions({
+		gfm: true,
+		tables: true,
+		breaks: true,
+		pedantic: false,
+		sanitize: false,
+		smartLists: true,
+		langPrefix: 'language-',
+		highlight: function(code, lang) {
+			if (lang === 'js') {
+				return highlighter.javascript(code);
 			}
-		});
+			return code;
+		}
+	});
 
-		$container.html(marked(window._markdown));
-
-
-		// Prettify
-		$('pre').each(function(i, el) {
-			el = $(el);
-			el.addClass('prettyprint lang-' + el.attr('class'));
-			el.html(el.html().trim());
-		});
-
-		prettyPrint();
-
-		$('pre').wrap('<div class="well" />');
+	$container.html(marked(window._markdown));
 
 
-		// Icons
-		var html = $container.html().replace(/\{\{([^ ]+)\}\}/g, '<i class="icon icon-$1"></i>');
-		$container.html(html);
+	// Prettify
+	$('pre').each(function(i, el) {
+		el = $(el);
+		el.addClass('prettyprint lang-' + el.attr('class'));
+		el.html(el.html().trim());
+	});
+
+	prettyPrint();
+
+	$('pre').wrap('<div class="well" />');
 
 
-		// Finished, show body
-		$body.show();
-	}, 1);
+	// Icons
+	var html = $container.html().replace(/\{\{([^ ]+)\}\}/g, '<i class="icon icon-$1"></i>');
+	$container.html(html);
+
+
+	// Finished, show body
+	$body.show();
 }
 
 // Load jquery and call init function
