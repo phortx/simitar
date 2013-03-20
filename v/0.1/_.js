bcc65db88b8687eda4bfdf25c491f6c11c107a17
phortx <benny@itws.de>
2013-03-20 16:17:27 +0100

    Finished 0.1

--- v/0.1/_.js
+++ v/0.1/_.js
@@ -2,25 +2,46 @@
 
 // Main init function, which requires jquery to be available
 function run() {
-	var $body = $('body');
+	var $body = $('body'),
+		$head = $('head');
 
 	var basePath = '';
 	$('script').each(function(i, script) {
 		script = $(script);
-		if (script.attr('src').match('/.*_.js$/')) {
+		if (script.attr('src').match('/_.js$')) {
 			basePath = script.attr('src');
 		}
 	});
-	
+
 	basePath = basePath.substr(0, basePath.lastIndexOf('/')) + '/';
 
-	
+
 	// Load resources
-	$body.append('<script src="' + basePath + 'marked.js"></script>')
+	$head.append('<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.no-icons.min.css" rel="stylesheet" />');
+	$head.append('<link href="//netdna.bootstrapcdn.com/font-awesome/3.0.2/css/font-awesome.css" rel="stylesheet" />');
+	$head.append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />');
+
+	$body.append('<script src="' + basePath + 'marked.js"></script>');
+	$body.append('<script src="' + basePath + 'prettify/prettify.js"></script>');
 	$body.append('<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>');
-	$body.append('<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet" />');
-	$body.append('<link href="' + basePath + 'simitar.css" rel="stylesheet" />');
-	$('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />')
+	$body.prepend('<div class="container"></div>');
+
+	var $container = $('.container');
+
+
+	// Load theme
+	if ($body.attr('class')) {
+		var theme = $body.attr('class').match('theme-([^ ]+)');
+
+		if (theme) {
+			theme = theme[1];
+			$head.append('<link href="//netdna.bootstrapcdn.com/bootswatch/2.3.1/' + theme + '/bootstrap.min.css" rel="stylesheet" />');
+		}
+	}
+
+	// Simitar and Prettify CSS
+	$head.append('<link href="' + basePath + 'prettify/prettify.css" rel="stylesheet" />');
+	$head.append('<link href="' + basePath + 'simitar.css" rel="stylesheet" />');
 
 
 	// Parse markdown
@@ -40,11 +61,28 @@ function run() {
 		}
 	});
 
-	$body.prepend(marked(window._markdown));
-	
+	$container.html(marked(window._markdown));
+
+
+	// Prettify
+	$('pre').each(function(i, el) {
+		el = $(el);
+		el.addClass('prettyprint lang-' + el.attr('class'));
+		el.html(el.html().trim());
+	});
+
+	prettyPrint();
+
+	$('pre').wrap('<div class="well" />');
+
+
+	// Icons
+	var html = $container.html().replace(/\{\{([^ ]+)\}\}/g, '<i class="icon icon-$1"></i>');
+	$container.html(html);
+
 
 	// Finished, show body
-	$body.fadeIn(200);
+	$body.show();
 }
 
 // Load jquery and call init function
@@ -55,10 +93,11 @@ function init() {
 	body.innerHTML = '';
 
 	var script = window.document.createElement("script");
+
 	var ready = function() {
 		run();
 	};
-	
+
 	// Internet Explorer
 	if (script.readyState) {
 		script.onreadystatechange = function() {
